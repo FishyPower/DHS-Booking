3189da4b5290bf14f3cc2d6066bcdf56b9464412
thepoppycat <47619363+thepoppycat@users.noreply.github.com>
2020-02-29 22:46:52 +0800

    added sort by time and event

--- view_booking.py
+++ view_booking.py
@@ -1,18 +1,27 @@
 import sqlite3
 from hash_algo import Hash
 
-def View_Booking(email):
+def View_Booking(email, sort_type):
     db_name = Hash(email) + '.db'
     conn = sqlite3.connect('Database Folder\{0}'.format(db_name))
     c = conn.cursor()
 
-    bookings = c.execute('SELECT Start_Time, Meet_Who, Ref_Code FROM My_Booking')
-    bookings = bookings.fetchall()
-
     # --- Sort by time ---
+    if sort_type=="time":
+        bookings = c.execute('SELECT Start_Time, Meet_Who, Ref_Code FROM My_Booking ORDER BY Start_Time')
+        bookings = bookings.fetchall()
 
-    # --- Missing Code ---
-
-    # --- Sort by Time ---
+    # --- Sort by event ---
+    elif sort_type=="event":
+        bookings = c.execute('SELECT Start_Time, Meet_Who, Ref_Code FROM My_Booking ORDER BY Ref_Code, Start_Time')
+        bookings = bookings.fetchall()
 
     return bookings
+
+# --- debugging purposes ---
+
+'''if __name__=="__main__":
+    print("Sort byt time: " +str( View_Booking("chiang.yuhsuan@dhs.sg", "time")))
+    print("Sort byt event, then time: " + str(View_Booking("chiang.yuhsuan@dhs.sg", "event")))
+'''
+
