810ae625269d8b15480f771d219d75d99ce50b3c
FishyPower <35266738+FishyPower@users.noreply.github.com>
2020-03-02 21:26:53 +0800

    Update view_booking.py

--- view_booking.py
+++ view_booking.py
@@ -8,13 +8,17 @@ def View_Booking(email, sort_type):
 
     # --- Sort by time ---
     if sort_type=="time":
-        bookings = c.execute('SELECT Start_Time, Meet_Who, Ref_Code FROM My_Booking ORDER BY Start_Time')
+        bookings = c.execute('SELECT Start_Time, End_Time, Meet_Who, Ref_Code FROM My_Booking ORDER BY Start_Time')
         bookings = bookings.fetchall()
+        for i in range(len(bookings)-1):
+            if bookings[i][-1] == bookings[i+1][-1] and bookings[i][1] == bookings[i+1][0]:
+                bookings[i+1][0] == bookings[i][0]
+                bookings.pop(i)
         return bookings
 
     # --- Sort by event ---
     elif sort_type=="event":
-        bookings = c.execute('SELECT Start_Time, Meet_Who, Ref_Code FROM My_Booking ORDER BY Ref_Code, Start_Time')
+        bookings = c.execute('SELECT Start_Time, End_Time, Meet_Who, Ref_Code FROM My_Booking ORDER BY Ref_Code, Start_Time')
         bookings = bookings.fetchall()
         events = [] # --- "3D" array ---
         prev_refcode = ""
@@ -24,6 +28,11 @@ def View_Booking(email, sort_type):
             else:  # --- continuing adding more bookings of the same event --
                 events[-1].append(booking)
             prev_refcode = events[-1][-1][-1]
+        for j in range(len(events)):
+            for i in range(len(events[j])-1):
+            if events[j][i][-1] == events[j][i+1][-1] and events[j][i][1] == events[j][i+1][0]:
+                events[j][i+1][0] == events[j][i][0]
+                events[j].pop(i)
         return events
         
 
@@ -32,4 +41,3 @@ def View_Booking(email, sort_type):
     print("Sort byt time: " +str( View_Booking("chiang.yuhsuan@dhs.sg", "time")))
     print("Sort byt event, then time: " + str(View_Booking("chiang.yuhsuan@dhs.sg", "event")))
 '''
-
